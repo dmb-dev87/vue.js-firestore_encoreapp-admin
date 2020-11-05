76331faaad35db8f42ea0a127eb0dd14819836fa
dmb-dev87 <dmbdev800@gmail.com>
2020-11-05 12:59:07 -0800

    [TopOneExpert][2020-11-05][change dashboard]

--- src/views/widgets/WidgetsFeature.vue
+++ src/views/widgets/WidgetsFeature.vue
@@ -1,49 +1,49 @@
 <template>
     <CRow>
         <CCol md="4" sm="4">
-            <CWidgetBrand
-                color="info"
-            >
-                <CIcon
-                    name="cil-media-play"
-                    height="52"
-                    class="my-4 mr-4"
-                />
-                <p class="my-4 text-left">
-                    CLICK TO WATCH <br/>
-                    ONBOARDING VIDEO
-                </p>
-            </CWidgetBrand>
+            <div data-v-8850d83e="" class="card">
+                <div class="card-header content-center text-white p-0 bg-info">
+                    <CIcon
+                        name="cil-media-play"
+                        height="52"
+                        class="my-4 mr-4"
+                    />
+                    <p class="my-4 text-left">
+                        CLICK TO WATCH <br/>
+                        ONBOARDING VIDEO
+                    </p>
+                </div>
+            </div>
         </CCol>
         <CCol md="4" sm="4">
-            <CWidgetBrand
-                color="primary"
-            >
-                <CIcon
-                    name="cil-envelope-closed"
-                    height="52"
-                    class="my-4 mr-4"
-                />
-                <p class="my-4 text-left">
-                    CLICK TO SEND <br/>
-                    EMAIL TO SUPPORT
-                </p>
-            </CWidgetBrand>
+            <div data-v-8850d83e="" class="card">
+                <div class="card-header content-center text-white p-0 bg-primary">
+                    <CIcon
+                        name="cil-envelope-closed"
+                        height="52"
+                        class="my-4 mr-4"
+                    />
+                    <p class="my-4 text-left">
+                        CLICK TO SEND <br/>
+                        EMAIL TO SUPPORT
+                    </p>
+                </div>
+            </div>
         </CCol>
         <CCol md="4" sm="4">
-            <CWidgetBrand
-                color="warning"
-            >
-                <CIcon
-                    name="cil-calculator"
-                    height="52"
-                    class="my-4 mr-4"
-                />
-                <p class="my-4 text-left">
-                    CLICK TO SEE <br/>
-                    YOUR BILLING PLAN
-                </p>
-            </CWidgetBrand>
+            <div data-v-8850d83e="" class="card">
+                <div class="card-header content-center text-white p-0 bg-warning">
+                    <CIcon
+                        name="cil-calculator"
+                        height="52"
+                        class="my-4 mr-4"
+                    />
+                    <p class="my-4 text-left">
+                        CLICK TO SEE <br/>
+                        YOUR BILLING PLAN
+                    </p>
+                </div>
+            </div>
         </CCol>
     </CRow>
 </template>
